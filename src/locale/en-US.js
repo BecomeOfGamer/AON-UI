286f8dbdf7c1091c7cfbe66e7046d5e26b03be32
Robby <ta7382@gmail.com>
2018-06-03 01:58:20 +0800

    Update locale

--- src/locale/en-US.js
+++ src/locale/en-US.js
@@ -1,5 +1,13 @@
 const en_US = { // eslint-disable-line
   'intl.hello': 'Hello',
   'intl.name': 'my name is {name}',
+  'intl.player.UnitName': 'UnitName',
+  'intl.player.CurrentMoveSpeed': 'CurrentMoveSpeed',
+  'intl.player.CurrentHP': 'HP',
+  'intl.player.CurrentAttackSpeed': 'CurrentAttackSpeed',
+  'intl.player.CurrentLevel': 'CurrentLevel',
+  'intl.player.CurrentAttack': 'CurrentAttack',
+  'intl.player.CurrentArmor': 'CurrentArmor',
+  'intl.player.CurrentAttackRange': 'CurrentAttackRange',
 }
 export default en_US // eslint-disable-line
