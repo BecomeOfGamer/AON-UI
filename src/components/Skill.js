5ac9898a4ce12650b5b6294eabc7e3035d300932
Robby <ta7382@gmail.com>
2018-06-04 14:07:26 +0800

    Update 技能升級, 但是有焦點失效的問題待處理

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -15,53 +15,56 @@ class Skill extends React.Component {
 
     return (
       <div className={styles['skill-group']}>
-        <ul className={styles['skill-list']}>
-          {this.props.Skill.map((path, index) => {
-            let datakey = '?'
-            switch (index) {
-              case 0:
-                datakey = 'W'
-                break
-              case 1:
-                datakey = 'E'
-                break
-              case 2:
-                datakey = 'R'
-                break
-              case 3:
-                datakey = 'T'
-                break
-              case 4:
-                datakey = 'R'
-                break
-              case 5:
-                datakey = 'D'
-                break
-              default:
-                break
-            }
-            return (
-              <li key={`skillupimg${index}`}>
-                <img
-                  className={[styles['skill-up'], SkillCanLevelUp[index] ? '' : styles.disable].join(' ')}
-                  onClick={() => this.dispatch({ type: 'player/skillLevelUp', payload: { api: unrealapi, id: `skillupimg${index + 1}` } })}
-                  src={'assets/plus.png'}
-                  alt={''}
-                />
-                <div className={styles.skill} data-key={datakey} >
-                  <img
-                    src={path}
-                    alt={path}
-                    data-tip
-                    data-for={`skilltip${index}`}
-                    onClick={() => this.dispatch({ type: 'player/skillLevelUp', payload: { api: unrealapi, id: `skillupimg${index + 1}` } })}
-                  />
-                  <Tooltip id={`skilltip${index}`} tooltip={SkillDetail[index]} />
+        <div className={styles['skill-list']}>
+          {
+            this.props.Skill.map((path, index) => {
+              let datakey = '?'
+              switch (index) {
+                case 0:
+                  datakey = 'W'
+                  break
+                case 1:
+                  datakey = 'E'
+                  break
+                case 2:
+                  datakey = 'R'
+                  break
+                case 3:
+                  datakey = 'T'
+                  break
+                case 4:
+                  datakey = 'R'
+                  break
+                case 5:
+                  datakey = 'D'
+                  break
+                default:
+                  break
+              }
+              return (
+                <div>
+                  <div key={`skill-up-${index}`}>
+                    <img
+                      className={[styles['skill-up'], SkillCanLevelUp[index] ? '' : styles.disable].join(' ')}
+                      src={'assets/plus.png'}
+                      alt={''}
+                      onClick={() => this.dispatch({ type: 'player/skillLevelUp', payload: { api: unrealapi, id: `skillupimg${index + 1}` } })}
+                    />
+                  </div>
+                  <div className={styles.skill} data-key={datakey} >
+                    <img
+                      src={path}
+                      alt={path}
+                      data-tip
+                      data-for={`skilltip${index}`}
+                    />
+                    <Tooltip id={`skilltip${index}`} tooltip={SkillDetail[index]} />
+                  </div>
                 </div>
-              </li>
-            )
-          })}
-        </ul>
+              )
+            })
+          }
+        </div>
       </div>
     )
   }
