922811a685acd20343456d3e290c290b40fdf130
Robby <ta7382@gmail.com>
2018-06-09 03:17:11 +0800

    Update ue4 payload

--- src/locale/zh-TW.js
+++ src/locale/zh-TW.js
@@ -4,6 +4,7 @@ const zh_TW = { // eslint-disable-line
   'intl.player.UnitName': '英雄名稱',
   'intl.player.CurrentMoveSpeed': '移動速度',
   'intl.player.CurrentHP': 'HP',
+  'intl.player.CurrentMP': 'MP',
   'intl.player.CurrentAttackSpeed': '攻擊速度',
   'intl.player.CurrentLevel': '等級',
   'intl.player.CurrentAttack': '攻擊',
