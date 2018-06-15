71168f3526e6fe4604f51d6c813d51f26b9d0a3f
Robby <ta7382@gmail.com>
2018-06-16 02:26:56 +0800

    Update - emit form rect

--- src/models/status.js
+++ src/models/status.js
@@ -1,3 +1,8 @@
+import UnrealAPI from '../api/UnrealAPI'
+import Element from '../interface/Element'
+
+const URAPI = new UnrealAPI()
+
 export default {
 
   namespace: 'status',
@@ -5,6 +10,8 @@ export default {
   state: {
     progress: 100,
     fps: 36,
+    URAPI: undefined,
+    Elements: [],
   },
 
   subscriptions: {
@@ -21,6 +28,34 @@ export default {
   reducers: {
     save(state, action) {
       return { ...state, ...action.payload }
+    },
+    connectAPI(state, { payload }) {
+      URAPI.connect(payload.dispatch)
+      return {
+        ...state,
+        URAPI: URAPI,
+      }
+    },
+    rect(state, { payload }) {
+      // const data = [
+      //   { id: 'menu', x: 250, y: 200, w: 120, h: 100 },
+      //   { id: 'skill', x: 650, y: 800, w: 600, h: 400 },
+      // ]
+      // const data = JSON.stringify({ data: payload.elements })
+
+      const data = []
+      payload.elements.forEach((element, index) => {
+        const { x, y, height, width } = element.rect
+        data.push(new Element(element.id, x, y, height, width))
+      })
+
+      const dataJSONString = JSON.stringify({ data: data })
+      URAPI.emit('UpdateUIRegion', dataJSONString);
+
+      return {
+        ...state,
+        Elements: dataJSONString,
+      }
     },
     progress(state, { payload }) {
       return {
